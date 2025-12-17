4d9d0c0ff3e06817aa6df5956ca2f5289f3e2214
moonseungju <msj9965@gmail.com>
2025-12-17 15:02:33 +0900

    chore: make project (#1)

---
+++ src/main/java/com/skyland/skylandchatbot/SkylandChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.skyland.skylandchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylandChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkylandChatbotApplication.class, args);
+    }
+
+}
